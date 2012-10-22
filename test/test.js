a5db9e0b81ffa7d5303aa41a7a8ad27c077d3b7a
willerce <willerce@gmail.com>
2012-10-23 00:05:31 +0800

    修复 status set 接口

--- test/test.js
+++ test/test.js
@@ -1,36 +1,36 @@
-var RenRen=require("../lib/index.js");
+var RenRen = require("../lib/index.js");
 
-var config={
+var config = {
     app_key:"99754adae54e49bc826da1144ad2659d",
     app_secret:"ddf05a79792a4a0aac6cfb42755e25c9",
     redirect_uri:"http://localhost:8080/sina_auth_cb"
 }
 
-var app_auth={
-    auth:function(req,res){ 
+var app_auth = {
+    auth:function (req, res) {
         var api = new RenRen(config);
         var auth_url = api.oauth.authorize();
-        res.redirect(auth_url); 
-        res.end();     
+        res.redirect(auth_url);
+        res.end();
     },
-    sina_auth_cb:function(req,res){
+    sina_auth_cb:function (req, res) {
         var code = req.query.code;
         var api = new RenRen(config);
-        api.oauth.accesstoken(code,function(data) {
+        api.oauth.accesstoken(code, function (data) {
             res.cookie("token", data.access_token);
             res.redirect('oauth');
-            res.end(); 
+            res.end();
         })
-       
-    }  
-} 
+
+    }
+}
 //import some libs 
 var express = require('express');
 var cons = require('consolidate');
 //init express app
 var app = express();
 app.use(express.logger({
-    format: ':method :url :status'
+    format:':method :url :status'
 }));
 //设置文件上传临时文件夹
 app.use(express.bodyParser({
@@ -38,14 +38,14 @@ app.use(express.bodyParser({
 }));
 app.use(express.cookieParser());
 app.use(express.session({
-    secret: 'yutou'
+    secret:'yutou'
 }));
 app.use(app.router);
 app.use(express.errorHandler({
-    dumpExceptions: true, 
-    showStack: true
+    dumpExceptions:true,
+    showStack:true
 }));
-app.error=function(err, req, res){
+app.error = function (err, req, res) {
     console.log("500:" + err + " file:" + req.url)
     res.render('500');
 }
@@ -55,23 +55,24 @@ app.engine("html", cons.mustache);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 app.set('view options', {
-    layout: false
-}) 
+    layout:false
+})
 app.listen("8080")
 //获取authorize url
-app.get("/auth",app_auth.auth)
+app.get("/auth", app_auth.auth)
 //获取accesstoken ,存储，并设置userid到cookie
-app.get("/sina_auth_cb",app_auth.sina_auth_cb)
+app.get("/sina_auth_cb", app_auth.sina_auth_cb)
 //中间页面，提醒用户认证成功
-app.get('/oauth', function(req, res){
+app.get('/oauth', function (req, res) {
     var api = new RenRen(config);
-    api.status.set({ 
-        status: ("hello"),
-        access_token: (req.cookies.token) 
-    },function(data){ 
-        console.log(data.toString()    )
-        res.render('oauth.html');     
+    api.status.set({
+        status:("hello"),
+        access_token:(req.cookies.token)
+    }, function (data) {
+        console.log(data);
+        var body = JSON.parse(data);
+        res.render('oauth.html');
     })
-   
+
 });
 
